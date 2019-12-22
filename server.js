d3000c1d0b6626ef77161757c12a399a47c69f44
kornif <kapnudel007@gmail.com>
2019-12-22 11:31:39 +0800

    .

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+//MONGO URI
+const uri = process.env.ATLAS_URI;
+
+//MONGODB Connection
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .catch(err => console.log(err));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+//USE ROUTES
+app.use("/api/items", items);
+
+//SERVE static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //SET a static folder
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", function(_, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"), function(
+      err
+    ) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
